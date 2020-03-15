a5ccfcfa441d9bf4cdcccc508d592563f7554fc5
Merg Knight <ninetynineknights@gmail.com>
2020-03-15 13:09:51 -0500

    update

---
+++ ex3_8seeingTheWorld.py
@@ -0,0 +1,22 @@
+placesToGo = ["vietnam", "houston", "japan", "california", "bahamas"]
+print(placesToGo)
+print(sorted(placesToGo))
+print(placesToGo)
+
+print(sorted(placesToGo, reverse=True))
+
+print(placesToGo)
+
+placesToGo.reverse()
+print(placesToGo)
+
+placesToGo.reverse()
+print(placesToGo)
+
+placesToGo.sort()
+print(placesToGo)
+
+placesToGo.sort(reverse=True)
+print(placesToGo)
+
+print(len(placesToGo))
