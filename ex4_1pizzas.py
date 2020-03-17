9ab13d4b9345929d06f80aa8112376c55fb0280f
Merg Knight <ninetynineknights@gmail.com>
2020-03-17 00:20:20 -0500

    hi

---
+++ ex4_1pizzas.py
@@ -0,0 +1,6 @@
+pizzaList = ["cheese", "pepperoni", "beef", "mushroom"]
+
+for pizzaType in pizzaList:
+    print("I love " + pizzaType + " pizza!")
+
+print("\nI really love pizza!!!")
---
+++ ex4_2animals.py
@@ -0,0 +1,6 @@
+animalList = ["shark", "lion", "wolf", "monster"]
+
+for animal in animalList:
+    print("A " + animal + " would make a great pet!")
+
+print("\noutside of the indention of the for loop")
