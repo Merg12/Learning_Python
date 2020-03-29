9fcec67b196a5e71beac40e00274fea88d578297
ninetynineknights <ninetynineknights@gmail.com>
2020-03-29 15:19:35 -0500

    hi

---
+++ ex6_1.favoriteNumbers.py
@@ -0,0 +1,9 @@
+favoriteNumbers = {
+    'carl' : 1,
+    'ceasar' : 18,
+    'chad' : 54,
+    'logan' : 32,
+    'trevor' : 7,
+}
+
+print("Carl's favorite number is " + str(favoriteNumbers['carl']))
--- ex6_1.py
+++ ex6_1.py
@@ -1,6 +1,11 @@
 Greg = {
-    'first name' : 'greg',
-    'last name' : 'gossman',
+    'first_name' : 'greg',
+    'last_name' : 'gossman',
     'age' : '40',
     'city' : 'houston',
     }
+
+print(Greg['first_name'].title() + " " + Greg['last_name'].title())
+print(Greg['last_name'].title())
+print(Greg['age'].title())
+print(Greg['city'].title())
