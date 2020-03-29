31bd439683c50c72d0746df597f73a952a782f64
ninetynineknights <ninetynineknights@gmail.com>
2020-03-28 19:38:38 -0500

    update

--- ex5_10checkingUsernames.py
+++ ex5_10checkingUsernames.py
@@ -1,9 +1,23 @@
-currentUsers = ['logan', 'alice', 'julie', 'harley', 'brad']
+currentUsers = ['logan', 'aliCe', 'Julie', 'hArley', 'braD']
 
-newUsers = ['matthew', 'alice', 'julie', 'avery', 'brad']
+newUsers = ['matthew', 'alice', 'Julie', 'avery', 'brad']
 
-for elem_newUsers in newUsers:
-    if elem_newUsers in currentUsers:
-        print("Sorry, " + elem_newUsers + " is currently in use")
-    else:
-        print("User " + elem_newUsers + " is good to go")
+print(currentUsers)
+
+for elem_currentUsers in currentUsers:
+    #print(elem_currentUsers)
+    newElem = elem_currentUsers.lower()
+    #print("Lowered: " + newElem)
+    #currentUsers.append(newElem) #adding this creates a recursion, it just keeps adding more to the list, and the for loop keeps running because every cycle adds more elements to the list
+    newUsers.append(newElem)
+
+print(newUsers)
+print(newElem)
+
+#    currentUsers.append(newElem)
+#    print(currentUsers)
+#for elem_newUsers in newUsers:
+#    if elem_newUsers.lower() in currentUsers:
+#        print("Sorry, " + elem_newUsers + " is currently in use, please enter a new username")
+#    else:
+#        print("User " + elem_newUsers + " is available")
