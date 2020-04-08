853858f6d92f63ae8b0ffd416b0c8cd4684a17d2
ninetynineknights <ninetynineknights@gmail.com>
2020-04-08 10:02:30 -0500

    j

---
+++ ex7_2.multiplesOfTen.py
@@ -0,0 +1,6 @@
+is_it_a_multiple_of_10 = input("please tell me a number and I'll tell you if its a multiple of 10!\n")
+
+if((int(is_it_a_multiple_of_10) % 10) == 0):
+    print("Yay it is!")
+else:
+    print("Nah...")
